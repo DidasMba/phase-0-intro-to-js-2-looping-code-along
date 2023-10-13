47e962ce45b809cca7934865fe0aab652351dfbe
DidasMba <daniel.mbarushi@gmail.com>
2023-10-13 21:24:50 +0300

    lopping completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// Fonction pour générer un message de remerciement pour chaque nom fourni
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Fonction pour compter à rebours à partir du nombre fourni jusqu'à zéro
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
